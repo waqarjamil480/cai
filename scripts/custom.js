f3209488544aa0fabc6e88ce6652b2ef8f251e51
waqarseedcode <68446951+waqarseedcode@users.noreply.github.com>
2020-09-22 21:57:11 +0500

    added some changes on U.i
    added some changes on U.i

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,4 +1,11 @@
-$(".year-ranges li").click(function () {
+$(".year-ranges li").click(function() {
     $(".year-ranges li").removeClass("active");
-    $(this).addClass("active");   
+    $(this).addClass("active");
+});
+
+$(function() {
+    $(".cus-btn-group .btn").click(function() {
+        $(".cus-btn-group .btn").removeClass("active");
+        $(this).addClass("active");
+    });
 });
